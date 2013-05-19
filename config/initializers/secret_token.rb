0b719d7442b7ffb650947b87951d2f83f3b049fe
engel lee <engel0088@gmail.com>
2013-05-20 06:33:49 +0800

    add file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mylife::Application.config.secret_token = '58299ef3e9534ae2eb1fc91fba7af19933a1190a23cc09f7676e4ba6c827c899fb9e8cc855a24f061d6f9dc4a21a5fe44383beacd5585d050b9c7129cd4650b7'
